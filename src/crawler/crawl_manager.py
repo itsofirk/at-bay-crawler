40bdefb8198a39e55c5f7e66b0b5f45ed3fa39bc
Tetro <itsofirk@gmail.com>
2024-01-16 00:40:38 +0200

    extracted crawl job logic to another class in crawl_manager.py - fixes queue bug

--- src/crawler/crawl_manager.py
+++ src/crawler/crawl_manager.py
@@ -24,27 +24,34 @@ class CrawlManager:
                 crawl_request = self.queue.get()
                 logger.info(f'Received crawl request: {crawl_request}')
                 try:
+                    job = CrawlJob(self.feed_storage, crawl_request)
                     # Process the crawl request asynchronously
-                    pool.apply_async(self.process_crawl_request, (crawl_request,))
+                    pool.apply(job.process_crawl_request)
                     logger.info(f'Crawl request added to processing queue. crawl_id: {crawl_request["crawl_id"]}')
                 except Exception as e:
                     logger.error(f'Error processing crawl request: {str(e)}')
 
-    def process_crawl_request(self, crawl_request):
-        crawl_id = crawl_request['crawl_id']
+
+class CrawlJob:
+    def __init__(self, feed_storage: BaseStorage, crawl_request: dict):
+        self.feed_storage = feed_storage
+        self.crawl_request = crawl_request
+
+    def process_crawl_request(self):
+        crawl_id = self.crawl_request['crawl_id']
+        logger.info(f'Processing crawl request: {crawl_id}')
         set_status(crawl_id, CrawlStatus.RUNNING.value)
-        self._start_scrapy_crawler(crawl_id, crawl_request)
+        self.start_scrapy_crawler(crawl_id)
         set_status(crawl_id, CrawlStatus.COMPLETE.value)
 
-    def _start_scrapy_crawler(self, job_id, crawl_requests):
+    def start_scrapy_crawler(self, job_id):
         try:
             process = CrawlerProcess(settings={
                 'FEEDS': {
                     'items.json': {'format': 'json'},
                 },
             })
-            for crawl_request in crawl_requests:
-                process.crawl(ScrapySpider, feed_storage=self.feed_storage, crawl_request=crawl_request)
+            process.crawl(ScrapySpider, feed_storage=self.feed_storage, crawl_request=self.crawl_request)
             process.start()
             logger.info(f'Scrapy crawler started successfully for crawl_id: {job_id}')
         except Exception as e:
