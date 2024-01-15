232f84046511f29928f4620dce9b1f9e0566bc5d
Tetro <itsofirk@gmail.com>
2024-01-15 21:21:19 +0200

    enabled concurrency mechanism limited by max_parallel_jobs

--- src/crawler/crawl_manager.py
+++ src/crawler/crawl_manager.py
@@ -2,17 +2,19 @@ from scrapy.crawler import CrawlerProcess
 from crawler.scrapy_spider import ScrapySpider
 from common.db import set_status
 from common.queue import queue
+from multiprocessing import Pool
 
 
 class CrawlManager:
-    def __init__(self, max_parallel_jobs: int = 5):
+    def __init__(self, max_parallel_jobs: int = 1):
         self.max_parallel_jobs = max_parallel_jobs
-        self.results = {}
+        self.pool = Pool(processes=max_parallel_jobs)  # Create a pool of worker processes
 
     def start_listening(self):
         while True:
             crawl_request = queue.get()  # Get the crawl request from the queue
-            self.process_crawl_request(crawl_request)
+            # Process the crawl request asynchronously
+            self.pool.apply_async(self.process_crawl_request, (crawl_request,))
 
     def process_crawl_request(self, crawl_request):
         crawl_id = crawl_request['crawl_id']
