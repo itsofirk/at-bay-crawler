d16330ac2f2322e65ae01e9ccc930b10ce8a03ed
Tetro <itsofirk@gmail.com>
2024-01-16 22:44:52 +0200

    refactored crawl_manager.py to the new BaseCrawler approach

--- src/crawler/crawl_manager.py
+++ src/crawler/crawl_manager.py
@@ -1,18 +1,32 @@
 import logging
 from multiprocessing import Pool
+from typing import Type
 
-from crawler.crawl_job import CrawlJob
-from infra.base_storage import BaseStorage
+from crawler.base_crawler import BaseCrawler
 
 logger = logging.getLogger(__name__)
 
 
 class CrawlManager:
-    def __init__(self, feed_storage: BaseStorage, queue, max_parallel_jobs: int = 1):
+    def __init__(self, crawler_class: Type[BaseCrawler], queue, max_parallel_jobs: int = 1, **crawler_kwargs):
         self.max_parallel_jobs = max_parallel_jobs
-        self.feed_storage = feed_storage
         self.queue = queue
+        self.crawler_class = crawler_class
+        self.crawler_kwargs = crawler_kwargs
+        self._validate_crawler_kwargs()
         logger.info('Crawl manager initialized successfully.')
+
+    def _validate_crawler_kwargs(self):
+        if 'start_url' in self.crawler_kwargs:
+            raise ValueError('start_url cannot be provided as a kwarg')
+        if 'crawl_id' in self.crawler_kwargs:
+            raise ValueError('crawl_id cannot be provided as a kwarg')
+
+    def _validate_crawl_request(self, crawl_request: dict):
+        if 'start_url' not in crawl_request:
+            raise ValueError('start_url must be provided in the crawl request')
+        if 'crawl_id' not in crawl_request:
+            raise ValueError('crawl_id must be provided in the crawl request')
 
     def start_listening(self):
         with Pool(processes=self.max_parallel_jobs) as pool:
@@ -21,9 +35,10 @@ class CrawlManager:
                 crawl_request = self.queue.get()
                 logger.info(f'Received crawl request: {crawl_request}')
                 try:
-                    job = CrawlJob(self.feed_storage, crawl_request)
+                    self._validate_crawl_request(crawl_request)
+                    crawler = self.crawler_class(**crawl_request, **self.crawler_kwargs)
                     # Process the crawl request asynchronously
-                    pool.apply(job.process_crawl_request)
+                    pool.apply(crawler.crawl)
                     logger.info(f'Crawl request added to processing queue. crawl_id: {crawl_request["crawl_id"]}')
                 except Exception as e:
                     logger.error(f'Error processing crawl request: {str(e)}')
